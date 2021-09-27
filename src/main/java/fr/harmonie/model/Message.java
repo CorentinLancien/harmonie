b2789a9f58337c97a56925131f568e7ba03d754b
unknown <c.lancien@iia-laval.fr>
2021-09-27 16:01:45 +0200

    init model

--- src/main/java/fr/harmonie/model/Message.java
+++ src/main/java/fr/harmonie/model/Message.java
@@ -1,5 +1,12 @@
 package fr.harmonie.model;
 
+import java.util.Date;
+
 public class Message {
 
+	private int id;
+	
+	private Date dateCrea;
+	
+	
 }
--- src/main/java/fr/harmonie/model/User.java
+++ src/main/java/fr/harmonie/model/User.java
@@ -1,5 +1,41 @@
 package fr.harmonie.model;
 
+import java.util.List;
+
 public class User {
 
+	private int id;
+	
+	private String pseudo;
+	
+	private List<Message> messages;
+
+	private List<Groupe> groupes;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public List<Message> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(List<Message> messages) {
+		this.messages = messages;
+	}
+	
+	
 }
